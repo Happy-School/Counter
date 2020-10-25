95e87eef80ef170b240d6d2ece5063b8d880e6fd
Happy School <ski.stumb@gmail.com>
2020-10-25 21:27:44 +0200

    Added Word counter

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spCounterType;
     private EditText inputTxt;
-    private TextView txtView;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +24,43 @@ public class MainActivity extends AppCompatActivity {
 
         this.spCounterType = findViewById(R.id.spCounterType);
         this.inputTxt = findViewById(R.id.inputTxt);
-        this.txtView = findViewById(R.id.txtView);
+        this.txtResult = findViewById(R.id.txtResult);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.counter_display, android.R.layout.simple_spinner_item);
 
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         this.spCounterType.setAdapter(adapter);
-
     }
-    public void btnToCount(View view) {
+    public void onBtnCount (View view) {
         Toast.makeText(this, this.spCounterType.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
 
-        String spCounterType = this.spCounterType.getSelectedItem().toString();
-        String charString = getString(R.string.display_chars);
+        String spSelectedOption = this.spCounterType.getSelectedItem().toString();
+        String charString = getString(R.string.selection_chars);
+        String wordsString = getString(R.string.selection_words);
 
-        if(spCounterType.equalsIgnoreCase(charString)){
-            this.txtView.setText(String.valueOf(this.inputTxt.getText().toString().length()));
+        if(spSelectedOption.equalsIgnoreCase(charString)){
+            String enteredUserText = this.inputTxt.getText().toString();
+            int charsCount = enteredUserText.length();
+
+            this.txtResult.setText(String.valueOf(charsCount));
         }
         else{
             Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT);
         }
+        if(spSelectedOption.equalsIgnoreCase(wordsString)){
+            String enteredUserText = this.inputTxt.getText().toString();
+            enteredUserText = enteredUserText.replace("\n"," ");
+            enteredUserText = enteredUserText.replace(","," ");
+            enteredUserText = enteredUserText.replace("."," ");
+            String[] textArray = enteredUserText.split(" ");
+
+            this.txtResult.setText(String.valueOf(textArray.length));
+        }
+        else{
+            Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT);
+        }
+
     }
 
 }
